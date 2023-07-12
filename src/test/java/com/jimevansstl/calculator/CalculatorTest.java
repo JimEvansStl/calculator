a35cf4b7b6b035bd6ca5780c91bb2dcf0819f79b
Jim Evans <jim.evans.career@gmail.com>
2023-07-12 14:01:04 -0500

    test running as expected

--- src/test/java/com/jimevansstl/calculator/CalculatorTest.java
+++ src/test/java/com/jimevansstl/calculator/CalculatorTest.java
@@ -1,11 +1,11 @@
 package com.jimevansstl.calculator;
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 public class CalculatorTest {
     private Calculator calculator = new Calculator();
     @Test
     public void testSum() {
-        assertEquals(10,5);
+        assertEquals(10,calculator.sum(1,9));
     }
 }
 
