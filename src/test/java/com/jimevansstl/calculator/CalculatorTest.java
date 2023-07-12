337e15afe6cced73ec54e5f4432e28a7d672357c
Jim Evans <jim.evans.career@gmail.com>
2023-07-11 22:41:50 -0500

    Add a test

---
+++ src/test/java/com/jimevansstl/calculator/CalculatorTest.java
@@ -0,0 +1,11 @@
+package com.jimevansstl.calculator;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+public class CalculatorTest {
+    private Calculator calculator = new Calculator();
+    @Test
+    public void testSum() {
+        assertEquals(10,5);
+    }
+}
+
